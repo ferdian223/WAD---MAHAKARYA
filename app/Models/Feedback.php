f95d0a4cc6cac46d4732c071bd181c60a8aa1e0b
ferdian223 <ferdaian.s14@gmail.com>
2024-12-31 19:09:57 +0700

    update lagi

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    protected $table = 'feedbacks';
+    protected $table = 'feedback';
     protected $fillable = ['name', 'message'];
 } 
